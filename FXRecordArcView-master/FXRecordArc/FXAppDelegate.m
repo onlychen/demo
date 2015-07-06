3bc21f64ce8405bb258528fdb8604afb20c12839
liang.chen <liang.chen@travelzen.com>
2015-07-06 16:42:10 +0800

    master分支

--- FXRecordArcView-master/FXRecordArc/FXAppDelegate.m
+++ FXRecordArcView-master/FXRecordArc/FXAppDelegate.m
@@ -13,6 +13,7 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     // Override point for customization after application launch.
+    //master分支
     return YES;
 }
 							
